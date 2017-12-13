34b578f7219308815bce40fba0eb246689c109ac
Edward <ea.litten@gmail.com>
2017-12-13 10:12:17 +0000

    Completed all exercises in step 8

--- directory.rb
+++ directory.rb
@@ -8,7 +8,7 @@ Exercises (done):
 5. Added hobbies & height, and print_expanded method to include them
 6. print_expanded uses ljust() to display student info prettyfied
 7. Modified input_students to take cohort too. Pressing enter twice still quits (unless cohort not yet entered)
-8. 
+8. print_by_cohort sorts students by cohort and outputs result
 9. Both input_student and footer adjust student(s) depending on # of students
 10. Changed input_students to use delete.("\n") instead of chomp
 11.
@@ -18,13 +18,13 @@ Exercises (done):
 students = [
     {name: "Dr. Hannibal Lecter", cohort: :november, hobbies: "Cannibalism", height: "1.8m"},
     {name: "Darth Vader", cohort: :november, hobbies: "Ruling the galaxy", height: "1.9m"},
-    {name: "Nurse Ratched", cohort: :november, hobbies: "Scheming", height: "1.65m"},
+    {name: "Nurse Ratched", cohort: :october, hobbies: "Scheming", height: "1.65m"},
     {name: "Michael Corleone", cohort: :november, hobbies: "Organised crime", height: "1.7m"},
-    {name: "Alex DeLarge", cohort: :november, hobbies: "Ultraviolence", height: "1.75m"},
+    {name: "Alex DeLarge", cohort: :october, hobbies: "Ultraviolence", height: "1.75m"},
     {name: "The Wicked Witch of the West", cohort: :november, hobbies: "Witchcraft", height: "1.5m"},
     {name: "Terminator", cohort: :november, hobbies: "Time travel", height: "1.9m"},
     {name: "Freddy Krueger", cohort: :november, hobbies: "Oneirology", height: "1.7m"},
-    {name: "The Joker", cohort: :november, hobbies: "Practical jokes", height: "1.85m"},
+    {name: "The Joker", cohort: :december, hobbies: "Practical jokes", height: "1.85m"},
     {name: "Joffrey Baratheon", cohort: :november, hobbies: "Cruelty", height: "1.6m"},
     {name: "Norman Bates", cohort: :november, hobbies: "Dissociative identities", height: "1.75m"}
   ]
@@ -34,7 +34,6 @@ def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
     students =[]
-    
     while true
         print "Name: "
         name = gets.delete("\n")
@@ -79,17 +78,27 @@ def print_expanded(students)
         puts "#{student[:name]} (#{student[:cohort]} cohort)".ljust(50) + "Likes: #{student[:hobbies]}".ljust(40) + "#{student[:height]} tall".ljust(20) }
 end
 
+def print_by_cohort(students)
+    sorted_by_cohort = {}
+    students.each {|student|
+        cohort = student[:cohort]
+        sorted_by_cohort[cohort] = [] if sorted_by_cohort[cohort].nil?
+        sorted_by_cohort[cohort] << student[:name]
+        }
+    sorted_by_cohort.each {|k,v| puts "#{k} cohort:"; puts v}   
+end
 
 def print_footer(students)
     puts "Overall we have #{students.count} great student#{students.count > 1 ? "s" : ""}" unless students.length == 0
 end
 
-students = input_students
+#students = input_students
 
 print_header
-print_names(students)
+#print_names(students)
 #print_spec_letter(students,"t")
 #print_spec_length(students,12)
 #print_without_each(students)
 #print_expanded(students)
+print_by_cohort(students)
 print_footer(students)

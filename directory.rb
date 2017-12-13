dae8b7bffc29038ea3abb7766ecbcd88dd85d618
Edward <ea.litten@gmail.com>
2017-12-13 10:30:41 +0000

    Added interactive menu

--- directory.rb
+++ directory.rb
@@ -15,6 +15,7 @@ Exercises (done):
 12. Won't print footer if there are no students (printing empty list in print_names doesn't matter)
 =end
 
+=begin Pre-made array of students for testing
 students = [
     {name: "Dr. Hannibal Lecter", cohort: :november, hobbies: "Cannibalism", height: "1.8m"},
     {name: "Darth Vader", cohort: :november, hobbies: "Ruling the galaxy", height: "1.9m"},
@@ -28,12 +29,11 @@ students = [
     {name: "Joffrey Baratheon", cohort: :november, hobbies: "Cruelty", height: "1.6m"},
     {name: "Norman Bates", cohort: :november, hobbies: "Dissociative identities", height: "1.75m"}
   ]
-      
+=end      
 
 def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
-    students =[]
     while true
         print "Name: "
         name = gets.delete("\n")
@@ -44,6 +44,28 @@ def input_students
         puts "Now we have #{students.count} student#{students.count > 1 ? "s" : ""}"
     end
     students
+end
+
+def interactive_menu
+    students =[]
+    loop do
+        puts "1. Input the students"
+        puts "2. Show the students"
+        puts "9. Exit" 
+        selection = gets.chomp
+        case selection
+            when "1"
+                students = input_students
+            when "2"
+                print_header
+                print_names(students)
+                print_footer(students)
+            when "9"
+                exit # this will cause the program to terminate
+            else
+                puts "I don't know what you meant, try again"
+        end        
+    end
 end
 
 def print_header
@@ -94,11 +116,11 @@ end
 
 #students = input_students
 
-print_header
+#print_header
 #print_names(students)
 #print_spec_letter(students,"t")
 #print_spec_length(students,12)
 #print_without_each(students)
 #print_expanded(students)
-print_by_cohort(students)
-print_footer(students)
+#print_by_cohort(students)
+#print_footer(students)
